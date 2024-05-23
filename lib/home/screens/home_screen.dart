53f44a5e8ff20fe65dfc2235e0f18ba50e2dd0b8
rameshr <rameshr@kpost.in>
2024-05-23 22:20:00 +0530

    code update

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -12,29 +12,90 @@ class HomeScreen extends GetWidget<HomeController> {
         backgroundColor: Colors.blue,
         title: const Text('Home Screen'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                  child: const Text('hello'),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                  child: const Text('hello'),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Card(
+                      elevation: 20,
+                      shadowColor: Colors.black,
+                      color: Colors.greenAccent[100],
+                      child: const SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Center(child: Text('Bus Timing')),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 50,
+                    shadowColor: Colors.black,
+                    color: Colors.greenAccent[100],
+                    child: const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Text('News')),
+                    ),
+                  ),
+                  Card(
+                    elevation: 50,
+                    shadowColor: Colors.black,
+                    color: Colors.greenAccent[100],
+                    child: const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Text('')),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Card(
+                    elevation: 20,
+                    shadowColor: Colors.black,
+                    color: Colors.greenAccent[100],
+                    child: const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Text('')),
+                    ),
+                  ),
+                  Card(
+                    elevation: 50,
+                    shadowColor: Colors.black,
+                    color: Colors.greenAccent[100],
+                    child: const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Text('')),
+                    ),
+                  ),
+                  Card(
+                    elevation: 50,
+                    shadowColor: Colors.black,
+                    color: Colors.greenAccent[100],
+                    child: const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Text('')),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
